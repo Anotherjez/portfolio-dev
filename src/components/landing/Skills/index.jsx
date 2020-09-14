050c9c60e9fc133d4f06115dda770cd7da5b9abb
anotherjez <anotherjez2@gmail.com>
2020-09-13 20:05:16 -0400

    adding skills

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -1,14 +1,28 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from 'providers/ThemeProvider';
-import { Container } from 'components/common';
+import { Container, Card } from 'components/common';
 import dev from 'assets/illustrations/skills.svg';
-import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles';
+import { Wrapper, SkillsWrapper, Details, Thumbnail, Grid, Item, Content } from './styles';
+import skills from './skills.json';
 
 export const Skills = () => {
   const { theme } = useContext(ThemeContext);
 
   return (
     <Wrapper id="about">
+      <h1>Skills</h1>
+      <Grid>
+        {skills.map(({ id, skillname, porcentaje }) => (
+          <Item key={id} as="div" theme={theme}>
+            <Card theme={theme}>
+              <Content>
+                <h4>{skillname}</h4>
+                <p>{porcentaje}%</p>
+              </Content>
+            </Card>
+          </Item>
+        ))}
+      </Grid>
       <SkillsWrapper as={Container}>
         <Thumbnail>
           <img src={dev} alt="I’m Jesus and I’m a Fullstack developer!" />
--- src/components/landing/Skills/styles.js
+++ src/components/landing/Skills/styles.js
@@ -63,3 +63,38 @@ export const Thumbnail = styled.div`
     width: 100%;
   }
 `;
+
+export const Grid = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 8fr;
+  gap: 1.2rem 1.2rem;
+
+  @media (max-width: 960px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 680px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const Item = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.11);
+
+  h4 {
+    color: ${({ theme }) => (theme === 'light' ? '#212121' : '#fff')};
+  }
+
+  p {
+    color: ${({ theme }) => (theme === 'light' ? '#707070' : '#c7c7c7')};
+  }
+`;
+
+export const Content = styled.div`
+  padding: 1rem 0;
+`;
